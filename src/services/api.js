647d10ea71cab6d8960d6c88858cfc6a1f833eae
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-17 23:19:48 +0530

    add backend server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api'; // change if deployed
+const API_BASE = 'https://leaderboard-backend.onrender.com/api'; // change if deployed
 
 export const fetchUsers = () => axios.get(`${API_BASE}/users`);
 export const addUser = (name) => axios.post(`${API_BASE}/users`, { name });
